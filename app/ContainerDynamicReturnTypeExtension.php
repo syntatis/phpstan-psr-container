2b638aca68017719f85faf7f8c6c6cab22cab664
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-01-28 08:24:43 +0700

    Add PHPCS

---
+++ app/ContainerDynamicReturnTypeExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syntatis\PHPStan\Psr11;
+
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use Psr\Container\ContainerInterface;
+
+use function count;
+use function is_string;
+use function ltrim;
+use function str_ends_with;
+
+class ContainerDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+	private ?string $interface = null;
+
+	public function __construct(?string $interface = null)
+	{
+		$interface = is_string($interface) ? ltrim($interface, '\\') : null;
+
+		if (! str_ends_with($interface, '\\' . ContainerInterface::class)) {
+			return;
+		}
+
+		$this->interface = $interface;
+	}
+
+	public function getClass(): string
+	{
+		return $this->interface ?? ContainerInterface::class;
+	}
+
+	public function isMethodSupported(MethodReflection $reflection): bool
+	{
+		return $reflection->getName() === 'get';
+	}
+
+	public function getTypeFromMethodCall(MethodReflection $reflection, MethodCall $methodCall, Scope $scope): Type
+	{
+		$args = $methodCall->getArgs();
+		if (count($args) === 0) {
+			return ParametersAcceptorSelector::selectFromArgs($scope, $methodCall->getArgs(), $reflection->getVariants())->getReturnType();
+		}
+
+		$arg = $args[0]->value;
+		// Care only for ::class parameters, we can not guess types for random strings.
+		if (! $arg instanceof ClassConstFetch) {
+			return ParametersAcceptorSelector::selectFromArgs($scope, $methodCall->getArgs(), $reflection->getVariants())->getReturnType();
+		}
+
+		$argType = $scope->getType($args[0]->value);
+		if (! $argType instanceof ConstantStringType) {
+			return ParametersAcceptorSelector::selectFromArgs($scope, $methodCall->getArgs(), $reflection->getVariants())->getReturnType();
+		}
+
+		return new ObjectType($argType->getValue());
+	}
+}
